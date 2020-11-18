9d0f458e806e67dfb4a9005aecf275411ef748b6
Natalia <nataliav_4428@hotmail.com>
2020-11-17 21:55:05 -0600

    search pokemon by name

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 import pokemon from './pokemon/pokemon.js';
 import data from './pokemon/pokemon.js'
 
-//Selectores
 
-// let start = document.querySelector('#start')
+let start = document.querySelector('#start')
 const container = document.querySelector('#container');
 const list = data.pokemon;
 console.log('You have connected...')
 console.log(pokemon)
-console.log(data.pokemon)
+const formulario = document.querySelector(".searchField");
+const boton = document.querySelector(".searchBtn");
+// const resultado = document.querySelector("#container");
+// 
 
+//Show all pokemons
+function drawAllPokemons() {
 
-function drawPokemons() {
-
-  container.innerHTML = "" // borrar! antes al principio, no dentro del ciclo
-      // dibujar
-  list.forEach((poke) => {
+  container.innerHTML = "" 
+     
+  list.forEach((pokemon) => {
       const div = document.createElement('div');
       div.innerHTML = `
-      <img src="${poke.img}" />
-      <h3>${poke.name}</h3>
-      <p>${poke.id}</p>
-      <p>${poke.type}</p>
+      <div class="card-container">
+      <img src="${pokemon.img}" />
+      <h3>${pokemon.name}</h3>
+      <p>${pokemon.id}</p>
+      <p>${pokemon.type}</p>
+      </div>
 `  ;
       container.appendChild(div);
 
@@ -29,69 +33,85 @@ function drawPokemons() {
   });
 }
 
+start.addEventListener('click', drawAllPokemons)
 
 
-// start.addEventListener('click', drawPokemons)
+formulario.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
 
-// 1.- nodo
-let typeFilterButtons = document.querySelectorAll('.buttons');
-// 2.- listener
-typeFilterButtons.forEach((buttons) => buttons.addEventListener('click', filterBichos));
-// 3.- callback
-function filterBichos(event) {
-    let id = event.target.id;
-    let filteredList = filterByType(id);
-    console.log(id)
-    console.log(filteredList);
- drawPokemons(filteredList) // [{},{}]
-
-        // aqui 
-}
-// 4.- func aux
-function filterByType(type) {
-    return data.pokemon.filter((poke) => poke.type.includes(poke.type))
-}
-
-
-const formulario = document.querySelector(".searchField");
-const boton = document.querySelector(".searchBtn");
-const resultado = document.querySelector("#container");
-const filtrar = () => {
-  
-  resultado.innerHTML = "";
-  console.log(formulario.value);
-  const texto = formulario.value.toLowerCase();
-  console.log(formulario.value.toLowerCase())
-  for (let poke of list) {
-      let names = poke.name.toLowerCase()
-      if (names.indexOf(texto) !== -1)
-
-
-          drawPokemons(resultado);
-
-
-      if (resultado.innerHTML === "")
-          resultado.innerHTML += ` Pokémon no encontrado... `;
-
-
-
-  };
-}
-
-boton.addEventListener("click", filtrar)
-
-function getShortMessages(name) {
-    return name
-      .filter(function(obj) {
-        return obj.name.length <= 5;
-      })
-      .map(function(obj) {
-        return obj.name;
-        console.log(obj.name)
+  console.log(e.target.value)
+  const filteredPoke = list.filter((pokemon) => {
+     return(
+          pokemon.name.toLowerCase().includes(searchString)
+     )
       });
-  }
+  console.log(filteredPoke)
+  drawPokemons(filteredPoke)
+
+  boton.addEventListener('click',filteredPoke)
+
+  drawPokemons(filteredPoke)
+
+})
+
+const drawPokemons = (list) => {
+    const htmlString = list.map((pokemon) => {
+            return `
+            <div class="card-container">
+            <li class="pokemon">
+                <h2>${pokemon.name}</h2>
+                <p>Type: ${pokemon.type}</p>
+                 <img src="${pokemon.img}" />
+                </li>
+                </div>
+        `;
+        })
+        .join('');
+    container.innerHTML = htmlString;
+};
 
 
-  boton.addEventListener("click", getShortMessages)
+// // 1.- nodo
+ let typeFilterButtons = document.querySelectorAll('.buttons');
+// // // 2.- listener
 
+ typeFilterButtons.forEach((buttons) => buttons.addEventListener('click', filterBichos));
  
+// // // 3.- callback
+function filterBichos(event) {
+  event.preventDefault();
+     let id = event.target.id;
+     let filteredList = filterByType(id);
+     console.log(id)
+     console.log(filteredList)
+     drawPokemons(filterByType) // [{},{}]
+
+
+function filterByType(type){
+  return list.filter((pokemon) => pokemon.type.includes(type))
+ };
+
+}
+// const filteredPoke = list.filter((pokemon) => {
+//   return(
+//        pokemon.name.toLowerCase().includes(searchString)
+//   )
+//    });
+// console.log(filteredPoke)
+// drawPokemons(filteredPoke)
+
+     
+
+
+// //         // aqui 
+ //}
+// // 4.- func aux
+// function filterByType(type) {
+//   console.log(type)
+
+//      return list.filter((pokemon) => pokemon.type.includes(type))
+//  }    
+//  console.log(filterByType())
+// drawAllPokemons(filterByType)
+
+
