def1f731f48c852ccd5d3cece77ec504dc954b78
Natalia <nataliav_4428@hotmail.com>
2020-11-26 17:40:33 -0600

    styles an functions completed

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ import data from './pokemon/pokemon.js'
 let start = document.querySelector('#start')
 const container = document.querySelector('#container');
 const list = data.pokemon;
-console.log('You have connected...')
-console.log(pokemon)
 const formulario = document.querySelector(".searchField");
 const boton = document.querySelector(".searchBtn");
 
 
 //Show all pokemons
+
 function drawAllPokemons() {
 
   container.innerHTML = ""
@@ -19,33 +18,33 @@ function drawAllPokemons() {
   list.forEach((pokemon) => {
     const div = document.createElement('div');
     div.innerHTML = `
-      <div class="card-container">
-      <img src="${pokemon.img}" />
-      <h3>${pokemon.name}</h3>
-      <p>${pokemon.id}</p>
-      <p>${pokemon.type}</p>
-      </div>
-      `  ;
+    <div class="card-block">
+    <ul class="pokemon">    
+    <div class="card-header">${pokemon.id}</div>
+    <div class="card-body">
+    <h2 class="card-title">${pokemon.name}</h2>
+    <p class="card-text">Type: ${pokemon.type}</p>
+    <img class="card-text" src="${pokemon.img}"/>
+    </ul>
+    </div>
+    `  ;
     container.appendChild(div);
   });
 }
 
+start.addEventListener('click', drawAllPokemons)
+
 
 //search Pokemon by Name
-start.addEventListener('click', drawAllPokemons)
 
 
 formulario.addEventListener('keyup', (e) => {
   const searchString = e.target.value.toLowerCase();
-
-  console.log(e.target.value)
   const filteredPoke = list.filter((pokemon) => {
     return (
       pokemon.name.toLowerCase().includes(searchString)
     )
   });
-  console.log(filteredPoke)
-  drawPokemons(filteredPoke)
 
   boton.addEventListener('click', filteredPoke)
 
@@ -56,19 +55,21 @@ formulario.addEventListener('keyup', (e) => {
 const drawPokemons = (list) => {
   const htmlString = list.map((pokemon) => {
     return `
-            <div class="card-container">
-            <ul class="pokemon">
-                <h2>${pokemon.name}</h2>
-                <p>Type: ${pokemon.type}</p>
-                 <img src="${pokemon.img}" />
-                </ul>
-                </div>
-        `;
+   
+    <div class="card-block">
+    <ul class="pokemon">
+    <div class="card-header">${pokemon.id}</div>
+    <div class="card-body">
+    <h2 class="card-title">${pokemon.name}</h2>
+    <p class="card-text">Type: ${pokemon.type}</p>
+    <img class="card-text" src="${pokemon.img}" />
+    </ul>
+    </div>           
+    `;
   })
     .join('');
   container.innerHTML = htmlString;
 };
-
 
 
 // Filter by type
@@ -80,8 +81,6 @@ function filterBichos(event) {
   event.preventDefault();
   let id = event.target.id;
   let filteredList = filterByType(id);
-  console.log(id)
-  console.log(filteredList)
   drawPokemons(filteredList) // [{},{}]
 
 
